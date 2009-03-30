ebeb3b62899b8c62a041e10b3c24e1aa4a2f30b6
shh42 <shh42@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-03-30 08:45:39 +0000

    Add Framework::Plone.
    git-svn-id: http://svn.plone.org/svn/collective/PASPlugins/Products.AutoRole/trunk@83414 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='Products.AutoRole',
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Plone",
         "Framework :: Zope2",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
